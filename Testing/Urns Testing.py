9a97390c2a9b573b9aa75c2efc9eb9efa7aee07a
aegoe <48519106+aegoe@users.noreply.github.com>
2020-07-13 17:35:11 +0200

    urn testing

--- Testing/Urns Testing.py
+++ Testing/Urns Testing.py
@@ -10,13 +10,72 @@ Urn_2 = [('Black', 1), ('Yellow', 3), ('Blue', 5), ('Green', 1)]
 
 Urn_3 = [('White', 1), ('Yellow', 5), ('Black', 3), ('Green', 1)]
 
+while True:
+    while True:
+        try:
+            Urn_1_input = input('How often do you want to draw from Option 1? ')
+            Urn_1_input_val = int(Urn_1_input)
+            break
+        except ValueError:
+            print('You can only choose a number between 0 and 10')
+
+    while True:
+        try:
+            Urn_2_input = input('How often do you want to draw from Option 2? ')
+            Urn_2_input_val = int(Urn_2_input)
+            break
+        except ValueError:
+            print('You can only choose a number between 0 and 10')
+
+    while True:
+        try:
+            Urn_3_input = input('How often do you want to draw from Option 3? ')
+            Urn_3_input_val = int(Urn_3_input)
+            break
+        except ValueError:
+            print('You can only choose a number between 0 and 10')
+
+    Urn_input_overall = Urn_1_input_val + Urn_2_input_val + Urn_3_input_val
+    if Urn_input_overall == 10:
+        break
+    elif Urn_input_overall < 10:
+        print('You need to spend all 10 points')
+    elif Urn_input_overall > 10:
+        print('You can only spend 10 points')
+
+
+print(Urn_1_input_val)
+print(Urn_2_input_val)
+print(Urn_3_input_val)
+
+# data_inputs = {}
+# data_inputs['Urn_1_input'] = Urn_1_input
+# data_inputs['Urn_2_input'] = Urn_2_input
+# data_inputs['Urn_3_input'] = Urn_3_input
+
+#print(data_inputs)
+
+    # for k, v in data_inputs.items():
+    #     try:
+    #         data_inputs[k] = int(v)
+    #         break
+    #     except ValueError:
+    #         print('You can only choose a number between 0 and 10')
+
+
+# print(data_inputs)
+
+
+
+
+
 Draw_1 = [val for val, cnt in Urn_1 for i in range(cnt)]
 Draw_2 = [val for val, cnt in Urn_2 for i in range(cnt)]
 Draw_3 = [val for val, cnt in Urn_3 for i in range(cnt)]
 
-draws_1 = random.choices(Draw_1, k=10)
-draws_2 = random.choices(Draw_2, k=10)
-draws_3 = random.choices(Draw_3, k=10)
+draws_1 = random.choices(Draw_1, k=Urn_1_input_val)
+draws_2 = random.choices(Draw_2, k=Urn_2_input_val)
+draws_3 = random.choices(Draw_3, k=Urn_3_input_val)
 
 #Draw_1 = random.choices(Urn_1, k = 10)
 #Draw_2 = random.choices(Urn_2, k = 10)
@@ -63,4 +122,4 @@ for i in data_counts.keys():
 #for values in count_1.values():
 #    payoff += values
 
-print(payoff)
+print('You total payoff for this round is: ', payoff)
