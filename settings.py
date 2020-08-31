5e2d0648df0c15c760f8c3c25c397387c3555b17
aegoe <48519106+aegoe@users.noreply.github.com>
2020-08-31 23:37:55 +0200

    'time'

--- settings.py
+++ settings.py
@@ -34,7 +34,7 @@ mturk_hit_settings = {
     'description': 'This HIT takes approximately 20 minutes to complete. Please accept this HIT only if you can commit to completing it right away and without interruption. You can earn a substantial bonus that exceeds the reward for this HIT.',
     'frame_height': 800,
     'template': 'global/mturk_template.html',
-    'minutes_allotted_per_assignment': 45,
+    'minutes_allotted_per_assignment': 60,
     'expiration_hours': 168,
     'grant_qualification_id': '3F4KLX19QVRYWWIPE260NYV94EYOER',
     'qualification_requirements': [
